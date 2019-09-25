e89902544d8322e2439be27a41136330139cf996
Chtibo <thib_vincent@hotmail.com>
2019-09-25 11:54:38 +0200

    PRODUCTS

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,16 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World! You're the bext"
+
+@app.route('/api/v1/products')
+def get_products():
+    PRODUCTS = [
+        { 'id': 1, 'name': 'Skello' },
+        { 'id': 2, 'name': 'Socialive.tv' }
+    ]
+    return jsonify(PRODUCTS)
+
